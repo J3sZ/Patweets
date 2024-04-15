fc495a66f612a8d90e485d650085ec9e2a2bbcd4
CesarCarias <cesarleonardocariasvasquez@gmail.com>
2024-04-15 12:03:00 -0400

    CustomUser added, superuser admin_2 registered

--- patweets/models.py
+++ patweets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -6,7 +7,5 @@ from django.db import models
 class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-   
-
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
