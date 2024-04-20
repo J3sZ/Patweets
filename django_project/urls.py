8556797d255625a8804108f492caff707e665d46
Oscar Telleria <theomjty@gmail.com>
2024-04-20 15:27:45 -0400

    homeview_added

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,6 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('patweets.urls')),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
--- patweets/views.py
+++ patweets/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def homeview(request):
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts':posts})
